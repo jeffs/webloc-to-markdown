ddeed1c5fff33e4a15ccfabbc490b4d718e8fbf7
Jeff Schwab <jeff@unbuggy.com>
2019-05-25 22:06:01 -0400

    Parse textual (XML) webloc files (not bplists)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+// Standard library
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+use std::{env, io};
+
+// Third party
+use regex::Regex;
+use walkdir::{DirEntry, WalkDir};
+
+fn is_webloc(entry: &DirEntry) -> bool {
+    entry
+        .path()
+        .extension()
+        .filter(|&ext| ext == "webloc")
+        .is_some()
+}
+
+fn find_weblocs(root: String) -> impl Iterator<Item = PathBuf> {
+    WalkDir::new(root)
+        .into_iter()
+        .flatten()
+        .filter(is_webloc)
+        .map(|entry| entry.path().to_path_buf())
+}
+
+fn extract_link(line: &str) -> Option<String> {
+    let re = Regex::new(r"^\t<string>(.*)</string>$").unwrap();
+    re.captures(&line)
+        .and_then(|captures| captures.get(1).map(|m| m.as_str().to_string()))
+}
+
+fn read_link(path: &Path) -> Option<String> {
+    match File::open(path) {
+        Ok(file) => {
+            let links = io::BufReader::new(file)
+                .lines()
+                .flatten()
+                .filter_map(|line| extract_link(&line))
+                .collect::<Vec<String>>();
+            match links.as_slice() {
+                [link] => Some(link.to_string()),
+                _ => None,
+            }
+        }
+        Err(_) => None,
+    }
+}
+
+fn get_stem(path: &Path) -> &str {
+    path.file_stem()
+        .and_then(|os_str| os_str.to_str())
+        .unwrap_or("(non_utf8_file_name)")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let paths: Vec<PathBuf> = env::args().skip(1).flat_map(find_weblocs).collect();
+    let link_opts = paths.iter().map(|path| read_link(&path));
+    for (path, link_opt) in paths.iter().zip(link_opts) {
+        match link_opt {
+            Some(link) => {
+                println!("* [{}]( {} )", get_stem(path), link);
+            }
+            None => {
+                eprintln!("warning: failed to parse {}", path.display());
+            }
+        }
+    }
 }
