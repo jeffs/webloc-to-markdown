5926f66a66d82bde11cdcb4002eafc3abb159b08
Jeff Schwab <jeff@unbuggy.com>
2019-05-29 13:25:48 -0400

    Cache compiled regex

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,24 @@ use std::{env, io};
 use regex::Regex;
 use walkdir::{DirEntry, WalkDir};
 
+struct Extractor {
+    re: Regex,
+}
+
+impl Extractor {
+    pub fn new() -> Extractor {
+        Extractor {
+            re: Regex::new(r"^\t<string>(.*)</string>$").unwrap(),
+        }
+    }
+
+    pub fn link(&self, line: &str) -> Option<String> {
+        self.re
+            .captures(&line)
+            .and_then(|captures| captures.get(1).map(|m| m.as_str().to_string()))
+    }
+}
+
 fn is_webloc(entry: &DirEntry) -> bool {
     entry
         .path()
@@ -24,19 +42,14 @@ fn find_weblocs(root: String) -> impl Iterator<Item = PathBuf> {
         .map(|entry| entry.path().to_path_buf())
 }
 
-fn extract_link(line: &str) -> Option<String> {
-    let re = Regex::new(r"^\t<string>(.*)</string>$").unwrap();
-    re.captures(&line)
-        .and_then(|captures| captures.get(1).map(|m| m.as_str().to_string()))
-}
-
 fn read_link(path: &Path) -> Option<String> {
+    let extract = Extractor::new();
     match File::open(path) {
         Ok(file) => {
             let links = io::BufReader::new(file)
                 .lines()
                 .flatten()
-                .filter_map(|line| extract_link(&line))
+                .filter_map(|line| extract.link(&line))
                 .collect::<Vec<String>>();
             match links.as_slice() {
                 [link] => Some(link.to_string()),
